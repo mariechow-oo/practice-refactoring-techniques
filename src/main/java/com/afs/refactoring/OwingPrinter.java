8ca15e98199e4d667c63c0e2265864048f562924
Marie Chow <chowma3@oocl.com>
2022-12-05 17:10:10 +0800

    refactor: extract calculateOutstanding

--- src/main/java/com/afs/refactoring/OwingPrinter.java
+++ src/main/java/com/afs/refactoring/OwingPrinter.java
@@ -5,23 +5,22 @@ import java.util.List;
 
 public class OwingPrinter {
     void printOwing(String name, List<Order> orders) {
-        Iterator<Order> elements = orders.iterator();
-        int outstanding = 0;
+        int outstanding = calculateOutstanding(orders);
 
         // print banner
         System.out.println ("*****************************");
         System.out.println ("****** Customer totals ******");
         System.out.println ("*****************************");
 
-        // print owing
-        while (elements.hasNext()) {
-            Order each = (Order) elements.next();
-            outstanding += each.getAmount();
-        }
-
         // print details
         System.out.println("name: " + name);
         System.out.println("amount: " + outstanding);
+    }
+
+    private int calculateOutstanding(List<Order> orders) {
+        return orders.stream()
+                .mapToInt(Order::getAmount)
+                .sum();
     }
 }
 
